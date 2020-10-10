29e0508e07c5e856663ab0413a4e09ba25d38490
daniyalnagori <daniyalnagori@yahoo.com>
2020-10-10 20:03:26 +0500

    addedee

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript"],
+  plugins: ["gatsby-plugin-typescript",
+  {
+    resolve: `gatsby-plugin-netlify-functions`,
+    options: {
+      functionsSrc: `${__dirname}/src/functions`,
+      functionsOutput: `${__dirname}/functions`,
+    },
+  },
+],
 };
